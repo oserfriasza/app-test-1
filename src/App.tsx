2ff7e7f5874ce8077782a538af33552e2fdc80d1
friaszavalzao3 <102121487+friaszavalzao3@users.noreply.github.com>
2022-10-07 19:01:01 -0600

    4.8 completado reduce

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 // import { Contador } from "./components/Contador";
-import { Contador } from "./components/Contador";
-import { ContadorHook } from "./components/ContadorHook";
+// import { Contador } from "./components/Contador";
+// import { ContadorHook } from "./components/ContadorHook";
+import { Login } from "./components/Login";
+//?Duda de este codigo
+const initialState = {
+  validando: true,
+  token: null,
+  username: '',
+  nombre: ''
+}
+
+
+//?Duda de este codigo
 const App = () => {
   return (
     <div>
       <h1>Introducción a Type Script with React</h1>
       <hr />
       {/* {<Contador />} */}
-      {<ContadorHook/>}
+      {/* {<ContadorHook/>} */}
+      {<Login/>}
     </div>
   );
 };
